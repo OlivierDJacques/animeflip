dd1ef6f09d5a4bfb90c4e3b148cc61476f815736
Dylan Tran <dylantranwork21@gmail.com>
2025-07-09 14:05:21 -0400

    testing: url perms

--- api/handleCORS.js
+++ api/handleCORS.js
@@ -5,7 +5,8 @@
  */
 function handleCORS(req, res) {
     const allowedOrigins = [
-        'http://127.0.0.1:5500/'
+        'http://127.0.0.1:5500',
+        'http://localhost:5173'
     ];
 
     const origin = req.headers.origin;  
